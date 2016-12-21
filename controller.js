9839b01cd985f8c2ddcebc7d870c5fd75c1089a6
ParimalDharane <ParimalDharane@users.noreply.github.com>
2016-12-21 15:39:02 +0530

    Fixed the issue by using 'class=mousetrap'
    Fixed issue - where hotkeys were not working after use of mouse

---
+++ controller.js
@@ -0,0 +1,51 @@
+app.controller('mainCtrl',
+    function mainCtrl($scope) {
+
+        $scope.currQ = 0;
+
+        $scope.qList = [
+            { question: 'Test Question A', response : '' }
+            ,{ question: 'Test Question B', response : '' }
+            ,{ question: 'Test Question C', response : '' }
+            ,{ question: 'Test Question D', response : '' }
+            ,{ question: 'Test Question E', response : '' }
+        ];
+
+        var selectResponse = function(respCode) {
+            $scope.qList[$scope.currQ].response = respCode;
+        };
+
+        $scope.setQuestion = function (newIndex) {
+            if(newIndex >= 0 || newIndex < $scope.qList.length) {
+                $scope.currQ = newIndex;
+            }
+        };
+
+        $scope.nextQuestion = function () {
+            if($scope.currQ < $scope.qList.length-1) {
+                $scope.currQ++;
+            }
+        };
+
+        $scope.prevQuestion = function () {
+            if($scope.currQ > 0) {
+                $scope.currQ--;
+            }
+        };
+
+        $scope.select1 = function () {
+            selectResponse('1');
+        };
+
+        $scope.select2 = function () {
+            selectResponse('2');
+        };
+
+        $scope.select3 = function () {
+            selectResponse('3');
+        };
+
+    }
+);
+
+
